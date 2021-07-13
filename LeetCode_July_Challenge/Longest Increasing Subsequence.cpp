a4dc6320a4760cb5c9d8c4f39a623d85eb11288b
Ayushi choudhary <67158285+ayushic2899@users.noreply.github.com>
2021-07-13 23:42:07 +0530

    Create Longest Increasing Subsequence.cpp

---
+++ LeetCode_July_Challenge/Longest Increasing Subsequence.cpp
@@ -0,0 +1,14 @@
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size(), ans = INT_MIN;
+        vector<int>dp(n, 1);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++)
+                if(nums[i] > nums[j]) dp[i] = max(dp[i], 1+dp[j]);
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+};
