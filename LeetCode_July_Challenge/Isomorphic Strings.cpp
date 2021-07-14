8fd32c30702cf974b3af86f6f277a4b8535d13b3
Ayushi choudhary <67158285+ayushic2899@users.noreply.github.com>
2021-07-14 11:51:44 +0530

    Update Isomorphic Strings.cpp

--- LeetCode_July_Challenge/Isomorphic Strings.cpp
+++ LeetCode_July_Challenge/Isomorphic Strings.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.length()!=t.size()){
+            return false;
+        }
+        unordered_map<char,char>mp1;
+        unordered_map<char,char>mp2;
+        for(int i=0;i<s.size();i++){
+            if(mp1[s[i]]==0 && mp2[t[i]]==0){
+                mp1[s[i]]=t[i];
+                mp2[t[i]]=s[i];
+            }
+            else if(mp1[s[i]]!=t[i]){
+                return false;
+            }
+        }
+        return true;       
+    }
+};
