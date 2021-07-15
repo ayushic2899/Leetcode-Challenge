b8994b7dbb5b0e5a01d0c11f8ac25e21d013b40b
Ayushi choudhary <67158285+ayushic2899@users.noreply.github.com>
2021-07-15 09:56:01 +0530

    Create Custom Sort String.cpp

---
+++ LeetCode_July_Challenge/Custom Sort String.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string customSortString(string order, string str) {
+        int  a[26]={0};
+         string s1,s2;
+        for(char c:order){
+            a[c-'a']++;
+        }
+        for(char c:str){
+            if(a[c-'a']==0){
+                s2+=c;
+            }
+            else{
+                a[c-'a']++;
+            }
+        }
+        for(char c:order){
+            while(a[c-'a']>1){
+                s1+=c;
+                a[c-'a']--;
+            }
+        }
+        return s1+s2;
+    }
+};
