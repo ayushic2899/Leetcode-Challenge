ccecc1f2a5979c7d440f6b9ce6292523f67a0811
Ayushi choudhary <67158285+ayushic2899@users.noreply.github.com>
2021-08-12 22:13:54 +0530

    Create Group Anagrams.cpp

---
+++ Leetcode_August_Challenge/Group Anagrams.cpp
@@ -0,0 +1,24 @@
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+````
+
+class Solution {
+public:
+      vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<string,vector<string>> m;
+        for(auto i: strs)
+        {
+            string temp=i;
+            sort(i.begin(),i.end());
+            m[i].push_back(temp);
+        }
+        vector<vector<string>> res;
+        for(auto it: m)
+        {
+            res.push_back(it.second);
+        }
+        return res;
+   
+    }
+};
