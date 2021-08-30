64f19825b53aab2632ab23cdd218fadd7069d269
Ayushi choudhary <67158285+ayushic2899@users.noreply.github.com>
2021-08-30 18:23:49 +0530

    Create Range Addition II.cpp

---
+++ Leetcode_August_Challenge/Range Addition II.cpp
@@ -0,0 +1,15 @@
+link-https://leetcode.com/problems/range-addition-ii/
+
+````````````
+
+
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+         for(int i=0;i<ops.size();i++){
+             m=min(m,ops[i][0]);
+             n=min(n,ops[i][1]);
+         }
+        return m*n;
+    }
+};
