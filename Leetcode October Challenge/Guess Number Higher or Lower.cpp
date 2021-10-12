214223162ffc0972cb0d0c063ae0e90ddf3374b4
Ayushi choudhary <67158285+ayushic2899@users.noreply.github.com>
2021-10-12 13:06:20 +0530

    Create Guess Number Higher or Lower.cpp

---
+++ Leetcode October Challenge/Guess Number Higher or Lower.cpp
@@ -0,0 +1,30 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int low=1;
+        while(low <= n){
+            int mid=low+(n-low)/2;
+            int res=guess(mid);
+            if(res==0){
+                return mid;
+            }
+            else if(res==-1){
+                n=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
+};
+ 
